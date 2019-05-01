9bab1f9caa6d1b52bf662aacfe560227ff0a24ad
Eilon Tsadok <37323667+eilon26@users.noreply.github.com>
2019-05-01 08:59:39 +0300

    Fix start new game short 7

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -140,7 +140,7 @@ void SmartGuesser::learnShort(string calculateBullAndPgia_ans) {
  }
 void SmartGuesser::startNewGameShort(unsigned int length) { 
 	AllPerms.clear();
-        &_guess = nullptr;
+        _guess.clear();
 	bullpgia::Guesser::startNewGame(length);
 	unsigned int max = pow(10,length);
 
